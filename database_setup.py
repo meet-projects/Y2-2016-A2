a6bebaba3cf31dec4fa66c6d98e159217901d097
inbal16@meet.mit.edu <inbal16@meet.mit.edu>
2016-08-02 17:23:53 +0300

    Started to add properties to object

--- database_setup.py
+++ database_setup.py
@@ -7,3 +7,13 @@ Base = declarative_base()
 
 #PLACE YOUR TABLE SETUP INFORMATION HERE
 
+class Person(Base):
+    __tablename__ = 'person'
+    name = Column(String)
+    id = Column(Integer, primary_key=True)
+    gender = Column(String)
+    nationality = Column(String)
+    hometown = Column(String)  # city/town name
+    email = Column(String)
+    phone = Column(String)
+    DOB = Column(String) #change
