b233c2e6a0da754177c464c32b3d5a5a9567902a
Berge Hagopian <bergehagopian1@hotmail.com>
2016-08-03 11:06:01 +0300

    Error message change

--- web_app.py
+++ web_app.py
@@ -15,9 +15,51 @@ session = DBSession()
 
 
 #YOUR WEB APP CODE GOES HERE
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('main_page.html')
+	if request.method == 'GET':
+		return render_template('main_page.html')
+	else:
+		email = request.form('email')
+		password = request.form('password')
+		user = session.query(Person).filter_by(email=email).first()
+		if password == user.password:
+			return render_template('search.html')
+		else:
+			return render_template('main_page.html', error = True)
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def add_friend():
+	if request.method == 'GET':
+		currentyear = datetime.now().year
+		years = range(currentyear, currentyear - 101, -1)
+		months = range(1, 13)
+		days = range(1, 32)
+		return render_template('sign_up.html', years = years, months = months, days = days)
+	else:
+		dob = datetime(int(request.form['year']), int(request.form['month']), int(request.form['day']))
+		friend = Person(name = request.form['name'],
+						gender = request.form['gender'],
+						nationality = request.form['nationality'],
+						city = request.form['city'],
+						email = request.form['email'],
+						password = request.form['password'],
+						instrument = request.form['instrument'],
+						genre = request.form['genre'],
+						phone = request.form['phone'],
+						dob = dob,
+						tv_shows = request.form['tv_shows'])
+
+		session.add(friend)
+		session.commit()
+
+		return redirect(url_for('search'))
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    return render_template('search.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
